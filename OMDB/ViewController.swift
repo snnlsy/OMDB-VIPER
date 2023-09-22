b76e2b0c74656bb68098396af5e79bd87cb267cf
Sinan Ulusoy <ssinanulusoy@gmail.com>
2023-09-22 23:01:16 +0300

    Initial Commit

---
+++ OMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OMDB
+//
+//  Created by Sinan Ulusoy on 22.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
